18eca25864c01587597ecc2046018576eb0c0a66
cozyxiong <cozyxiong@163.com>
2025-05-17 17:22:22 +0800

    init

---
+++ script/LSP.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+
+contract LSPScript is Script {
+
+    function run() public {
+        vm.startBroadcast();
+
+
+        vm.stopBroadcast();
+    }
+}
