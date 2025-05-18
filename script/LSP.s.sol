a320362313c521086e0bfb43f5a5742c8ae6685b
cozyxiong <cozyxiong@163.com>
2025-05-18 21:34:32 +0800

    fix bug

--- script/LSP.s.sol
+++ script/LSP.s.sol
@@ -1,14 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
 import {Script, console} from "forge-std/Script.sol";
 
 contract LSPScript is Script {
 
-    function run() public {
-        vm.startBroadcast();
-
-
-        vm.stopBroadcast();
-    }
 }
